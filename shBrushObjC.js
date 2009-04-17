4da429a8d445b5ff368dc75cdd57bf539d78616e
Monomachus <Monomachus@41a340c6-2b86-11de-a3db-a5b3f7bb893a>
2009-04-17 20:45:07 +0000

    Changed it to be compatible with the latest SH version
    git-svn-id: http://sh-objective-c.googlecode.com/svn/trunk@5 41a340c6-2b86-11de-a3db-a5b3f7bb893a

--- shBrushObjC.js
+++ shBrushObjC.js
@@ -1,56 +1,57 @@
 /**
- * Code Syntax Highlighter for Objective-C.
- * Version 0.0.2
- * Copyright (C) 2006 Shin, YoungJin.
- * http://www.jiniya.net/lecture/techbox/test.html
+ * SyntaxHighlighter - Objective-C Brush
+ * http://codepirate.seaandco.com/
+ *
+ * @version
+ * 1.0.0 (February 22 2009)
+ *
+ * @author
+ * Geoffrey Byers
  * 
- * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General 
- * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) 
- * any later version.
+ * @copyright
+ * Copyright (C) 2009 Geoffrey Byers.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
- * details.
+ * Licensed under a GNU Lesser General Public License.
+ * http://creativecommons.org/licenses/LGPL/2.1/
  *
- * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to 
- * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ *  Updated From:
+ *  Code Syntax Highlighter for Objective-C.
+ *  Version 0.0.2
+ *  Copyright (C) 2006 Shin, YoungJin.
+ *  http://scottdensmore.typepad.com/blog/2008/12/objective-c-cocoa-syntax-highlighter.html
  */
 
-dp.sh.Brushes.ObjC = function()
+SyntaxHighlighter.brushes.ObjC = function()
 {
-	var datatypes =
-	'char bool BOOL double float int ' +
-	'long short id void';
-
-	var keywords = 
-	//'@property @selector @interface @end @implementation @synthesize ' +
-	'IBAction IBOutlet SEL YES NO readwrite readonly nonatomic nil NULL ' +
-	'super self copy ' +
-	'break case catch class const copy __finally __exception __try ' +
-	'const_cast continue private public protected __declspec ' + 
-	'default delete deprecated dllexport dllimport do dynamic_cast ' + 
-	'else enum explicit extern if for friend goto inline ' + 
-	'mutable naked namespace new noinline noreturn nothrow ' + 
-	'register reinterpret_cast return selectany ' + 
-	'sizeof static static_cast struct switch template this ' + 
-	'thread throw true false try typedef typeid typename union ' + 
-	'using uuid virtual volatile whcar_t while';
-
+	var datatypes =	'char bool BOOL double float int long short id void';
+	
+	var keywords = 'IBAction IBOutlet SEL YES NO readwrite readonly nonatomic nil NULL ';
+	keywords += 'super self copy ';
+	keywords += 'break case catch class const copy __finally __exception __try ';
+	keywords += 'const_cast continue private public protected __declspec ';
+	keywords += 'default delete deprecated dllexport dllimport do dynamic_cast ';
+	keywords += 'else enum explicit extern if for friend goto inline ';
+	keywords += 'mutable naked namespace new noinline noreturn nothrow ';
+	keywords += 'register reinterpret_cast return selectany ';
+	keywords += 'sizeof static static_cast struct switch template this ';
+	keywords += 'thread throw true false try typedef typeid typename union ';
+	keywords += 'using uuid virtual volatile whcar_t while';
+	// keywords += '@property @selector @interface @end @implementation @synthesize ';
+	
+		
 	this.regexList = [
-		{ regex: dp.sh.RegexLib.SingleLineCComments,				css: 'comment' },			// one line comments
-		{ regex: dp.sh.RegexLib.MultiLineCComments,					css: 'comment' },			// multiline comments
-		{ regex: dp.sh.RegexLib.DoubleQuotedString,					css: 'string' },			// strings
-		{ regex: dp.sh.RegexLib.SingleQuotedString,					css: 'string' },			// strings
-		{ regex: new RegExp('^ *#.*', 'gm'),						css: 'preprocessor' },
-		{ regex: new RegExp(this.GetKeywords(datatypes), 'gm'),		css: 'datatypes' },
-		{ regex: new RegExp(this.GetKeywords(keywords), 'gm'),		css: 'keyword' },
-		{ regex: new RegExp('\\bNS\\w+\\b', 'g'),					css: 'keyword' },
-		{ regex: new RegExp('@\\w+\\b', 'g'),						css: 'keyword' }
+		{ regex: SyntaxHighlighter.regexLib.singleLineCComments,		css: 'comments' },		// one line comments
+		{ regex: SyntaxHighlighter.regexLib.multiLineCComments,		css: 'comments' },		// multiline comments
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },			// double quoted strings
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },			// single quoted strings
+		{ regex: new RegExp('^ *#.*', 'gm'),						css: 'preprocessor' },		// preprocessor
+		{ regex: new RegExp(this.getKeywords(datatypes), 'gm'),		css: 'datatypes' },		// datatypes
+		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' },			// keyword
+		{ regex: new RegExp('\\bNS\\w+\\b', 'g'),					css: 'keyword' },			// keyword
+		{ regex: new RegExp('@\\w+\\b', 'g'),						css: 'keyword' },			// keyword
 		];
-
-	this.CssClass = 'dp-objc';
-	this.Style =	'.dp-objc .datatypes { color: #2E8B57; font-weight: bold; }';
+	
 }
 
-dp.sh.Brushes.ObjC.prototype	= new dp.sh.Highlighter();
-dp.sh.Brushes.ObjC.Aliases	= ['obj-c', 'objc'];
+SyntaxHighlighter.brushes.ObjC.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.ObjC.aliases = ['objc', 'obj-c'];
